1e9b678ee3ccdaf3b536210bec21aca7081ef0e1
Diogo Alex <38763303+diogoalex01@users.noreply.github.com>
2018-04-29 22:09:23 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "dictionary.h"
+#include "Board.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,6 +11,17 @@ int main()
 	//variables
 	string thesaurusFile; //file that contains the dictionary
 		   //wordFile; //file that will store the processed word list
+	int rows, columns;
+
+	cout << "How many rows: ";
+	cin >> rows;
+
+	cout << "How many columns: ";
+	cin >> columns;
+
+	Board board(rows, columns);
+	board.make();
+	board.show();
 
 	cout << "Thesaurus file? ";
 	cin >> thesaurusFile;
@@ -21,7 +33,6 @@ int main()
 
 	return 0;
 }
-
 
 /*
 cwcreator
